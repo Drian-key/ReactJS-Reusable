1fccdf315ef9445a8cfd86c920e40609f09cdcb1
Drian-key <riskidrian1@gmail.com>
2022-11-14 22:27:50 +0700

    Install react dan apa itu jsx

---
+++ Belajar_ReactJS_Sekolah_Koding/example/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Contoh penggunaan jsx secara langsung
+const name = "Drian Tcho";
+
+function greet() {
+  return "Hi " + name;
+}
+
+const element = <h1> {greet(name)}!! </h1>;
+
+const master = ReactDOM.createRoot(document.getElementById("master"));
+master.render(element);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
