3d64113a9cbd28e8931d431fcbe53ae8e5fea0f6
Drian-key <riskidrian1@gmail.com>
2022-11-16 21:13:45 +0700

    State pada reactjs

--- Belajar_ReactJS_Sekolah_Koding/example/src/App.js
+++ Belajar_ReactJS_Sekolah_Koding/example/src/App.js
@@ -1,30 +1,62 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
 // Component - Properties
 
-function Age(props) {
-  return <span>umurnya {props.age}</span>;
+// function Age(props) {
+//   return <span>umurnya {props.age}</span>;
+// }
+
+// function Greeting(props) {
+//   return (
+//     <h1>
+//       Hallo {props.name} - <Age age={props.age} />
+//     </h1>
+//   );
+// }
+
+// STATE
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.start,
+    };
+  }
+
+  // Lifecycle
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval);
+  }
+
+  increase() {
+    // Update status time setiap detik
+    this.setState((state, props) => ({
+      time: parseInt(state.time) + 1,
+    }));
+  }
+
+  render() {
+    return <div>{this.state.time} Detik</div>;
+  }
 }
 
-function Greeting(props) {
-  return (
-    <h1>
-      Hallo {props.name} - <Age age={props.age} />
-    </h1>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Drian Tcho" age="17" />
-        <Greeting name="Miko" age="20" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Timer start="0" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
