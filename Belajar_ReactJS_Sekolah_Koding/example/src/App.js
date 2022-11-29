068dd852c650e41514fa139edc40f7fe9af6d63c
Drian-key <riskidrian1@gmail.com>
2022-11-30 05:46:45 +0700

    koneksi api dan menggunakan fetch

--- Belajar_ReactJS_Sekolah_Koding/example/src/App.js
+++ Belajar_ReactJS_Sekolah_Koding/example/src/App.js
@@ -1,44 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
-import List from "./List";
 
 class App extends Component {
+  // API
   constructor(props) {
     super(props);
     this.state = {
-      todoItem: "",
       items: [],
+      isLoading: true,
     };
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // mencegah reload page
-    this.setState({
-      items: [...this.state.items, this.state.todoItem],
-      // cara di atas merupakan cara untuk push data ke array
-      // ...this.state.items <-- data sebelumnya
-      // , this.state.todoItem <-- data yang baru
-      todoItem: "",
-    });
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      todoItem: event.target.value,
-    });
-  };
+  // funsi ini akan di jalankan ketika component sudah siap
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ items: data, isLoading: false }));
+  }
 
   render() {
+    const { items, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>LOADING...</p>;
+    }
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit} className="flex gap-4">
-          <input value={this.state.todoItem} onChange={this.handleChange} className="bg-blue-300 rounded border border-slate-900" />
-          <button className="bg-yellow-200 px-2 rounded hover:bg-blue-500 transition-all ease-in-out">Add</button>
-        </form>
-
-        <List items={this.state.items} />
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item.name}tes</li>
+          ))}
+        </ul>
       </div>
     );
   }
