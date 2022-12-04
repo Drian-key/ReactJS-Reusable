5a190e01f471ac8b83bb448e20d64c4344db6224
Drian-key <riskidrian1@gmail.com>
2022-12-04 20:28:21 +0700

    menggunakan state function / react hook ke dalam system login sederhana

--- parsinta/first-app/src/App.js
+++ parsinta/first-app/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 function App() {
+  // Bagian name
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [name, setName] = useState("");
+
+  // Bagian login
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user_pw, setUserPw] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -11,6 +17,19 @@ function App() {
     setName(`${firstname} ${lastname}`);
     setFirstname("");
     setLastname("");
+  };
+
+  const submitHandlerLogin = (e) => {
+    e.preventDefault();
+
+    if (username == "drian" && password == "123") {
+      setUserPw(`Berhasil Login`);
+    } else {
+      setUserPw(`Gagal Login`);
+    }
+
+    setUsername("");
+    setPassword("");
   };
 
   return (
@@ -46,25 +65,25 @@ function App() {
             <div className="card">
               <div className="card-header">Login state function</div>
               <div className="card-body">
-                <form>
+                <form onSubmit={submitHandlerLogin}>
                   <div className="mb-2">
                     <label htmlFor="username" className="form-label">
                       Username
                     </label>
-                    <input type="text" className="form-control" name="username" id="username" />
+                    <input type="text" className="form-control" value={username} onChange={(e) => setUsername(e.target.value)} name="username" id="username" />
                   </div>
                   <div className="mb-4">
                     <label htmlFor="password" className="form-label">
                       Password
                     </label>
-                    <input type="password" className="form-control" name="password" id="password" />
+                    <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" />
                   </div>
                   <button type="submit" className="btn btn-primary btn-block">
                     Login
                   </button>
                 </form>
               </div>
-              <div className="card-footer">Silahkan login</div>
+              <div className="card-footer">{user_pw ? user_pw : "Silahkan login"}</div>
             </div>
           </div>
         </div>
