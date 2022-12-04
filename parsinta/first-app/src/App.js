2ed1fdcb30531025b23364b40672cd477a231720
Drian-key <riskidrian1@gmail.com>
2022-12-04 13:08:33 +0700

    belajar membuat login dengan reactjs

--- parsinta/first-app/src/App.js
+++ parsinta/first-app/src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
       firstname: "",
       lastname: "",
       name: "",
+      username: "",
+      password: "",
+      user_pw: "",
     };
   }
 
@@ -17,21 +20,44 @@ class App extends Component {
     });
   };
 
+  changeHandlerSaya = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   submitHandler = (e) => {
     e.preventDefault();
 
     this.setState({
-      name: `${this.state.firstname} ${this.state.lastname}`,
+      name: `tes ${this.state.firstname} ${this.state.lastname}`,
     });
 
     this.state.firstname = "";
     this.state.lastname = "";
   };
 
+  submitHandlerSaya = (e) => {
+    e.preventDefault();
+
+    if (this.state.username == "drian" && this.state.password == "123") {
+      this.setState({
+        user_pw: `Berhasil Login`,
+      });
+    } else {
+      this.setState({
+        user_pw: `Gagal Login`,
+      });
+    }
+
+    this.state.username = "";
+    this.state.password = "";
+  };
+
   render() {
-    const { name } = this.state;
+    const { name, user_pw } = this.state;
     return (
-      <div className="p-5">
+      <div className="py-5">
         <div className="container">
           <div className="row">
             <div className="col-md-6">
@@ -57,6 +83,31 @@ class App extends Component {
                   </form>
                 </div>
                 <div className="card-footer">My name is {name ? name : "...."}</div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">Bikinan aku</div>
+                <div className="card-body">
+                  <form onSubmit={this.submitHandlerSaya}>
+                    <div className="mb-4">
+                      <label htmlFor="username" className="form-label">
+                        Username
+                      </label>
+                      <input type="text" name="username" id="username" className="form-control" value={this.state.username} onChange={this.changeHandlerSaya} />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="password" className="form-label">
+                        Password
+                      </label>
+                      <input type="password" name="password" id="password" className="form-control" value={this.state.password} onChange={this.changeHandlerSaya} />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Kirim
+                    </button>
+                  </form>
+                </div>
+                <div className="card-footer">{user_pw ? user_pw : "Silahkan login ..."}</div>
               </div>
             </div>
           </div>
